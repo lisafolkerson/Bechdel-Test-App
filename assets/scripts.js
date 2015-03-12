4afde2d3e7e68bde5c9d317f27313e7091bb1c2a
Lisa Folkerson <lisafolkerson@gmail.com>
2015-03-11 22:36:11 -0400

    new and improved if/else statement

--- assets/scripts.js
+++ assets/scripts.js
@@ -6,17 +6,7 @@ app.init = function(){
 	$( '.search' ).on( 'submit', function(e){
 		e.preventDefault();
 		var Title = $('.q').val();
-
-		// remove 'the', 'a', 'and', 'an', and/or 'or' from search query:
-
-		// if it is at the beginning of the title, completely remove
-		// var regEx = /(^)(the|a|and|an|or)(\s|$)/i;
-		// Title = Title.replace(regEx, '');
-
-		// if it is between two words, replace with a space
-		// var regEx = /(\s)(the|a|and|an|or)(\s)/i;
-		// Title = Title.replace(regEx, ' ');
-
+		$('').focus();
 		console.log(Title);
 		app.getMovie(Title);
 		//app.getMovie(Title);
@@ -43,9 +33,8 @@ app.getMovie = function(Title) {
 
 // loop to display movies that match search query
 app.displayTitle = function(results) {
-
+	// clear old results and form info so new information can be searched
 	$( '#displayResults' ).empty();
-
 	$('#film-input').val('');
 
 	var numMovies = results.length;
@@ -60,21 +49,24 @@ app.displayTitle = function(results) {
 		var div = $( '<div>' ).addClass( 'particularFilm' );
 		var p = $( '<p>' ).text( results[i].title );
 
+		// set variable to return in different instances
 		app.imgYes = '<img src="assets/images/noun_40214_cc.svg">' + '<p>PASS</p>';
 		app.imgNo = '<img src="assets/images/noun_43299_cc.svg">' + '<p>FAIL</p>';
+		app.imgNone = '<p>¯\_(ツ)_/¯ Looks like nothing matches. Try again.</p>';
 
 		// if/else for if movie passes or not
 		if ( rating == 3 ) {
 			//display affirmation
 			console.log( 'it passes!' );
 			div.append(app.imgYes);
+		} else if ( results == undefined || results == null || results.length == 0) {
+			// no results, try again
+			console.log( 'there are no results' );
+			div.append(app.imgNone);		
 		} else {
 			console.log( 'this movie may not have and strong female characters. Why don\'t you try something else.' );
 			//display negative reading
-			//&
-			//suggest another movie that has positive rating
 			div.append(app.imgNo);
-
 		}; // end if/else
 
 
@@ -82,15 +74,7 @@ app.displayTitle = function(results) {
 		// add div with all results to main page
 		$( '#displayResults' ).append(div);
 
-		// clear old results when a new query is submitted
-		// $( '.search' ).on( 'submit',
-   			 
-  //  		 });
-		// when text is added to search form stay visible
-
 		// make an array of different movie screen caps to cylce through as backgrounds  and be suggestions for movies to watch that pass the bechdel test
-
-
 
 	};
 }; //end app.dispayTitle():
@@ -98,5 +82,4 @@ app.displayTitle = function(results) {
 
 $(function(){
 	app.init();
-	// app.movie();
 });//end doc ready
